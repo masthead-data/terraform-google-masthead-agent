4f851efafaf9317e121b9a97f5419a633272e8c6
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-10-16 22:06:47 +0200

    attempt to avoid filter conflicts

--- modules/bigquery/main.tf
+++ modules/bigquery/main.tf
@@ -69,7 +69,15 @@ resource "google_logging_project_sink" "masthead_sink" {
   # Enhanced filter for comprehensive BigQuery monitoring
   filter = <<-EOT
 (
-  protoPayload.methodName="google.cloud.bigquery.storage.v1.BigQueryWrite.AppendRows" OR
+  (
+    protoPayload.methodName="google.cloud.bigquery.storage.v1.BigQueryWrite.AppendRows" AND
+    -protoPayload.resourceName=~"projects/httparchive/datasets/crawl"
+  ) OR
+  (
+    protoPayload.methodName="google.cloud.bigquery.storage.v1.BigQueryWrite.AppendRows" AND
+    protoPayload.resourceName=~"projects/httparchive/datasets/crawl" AND
+    sample(insertId, 0.001)
+  ) OR
   protoPayload.methodName="google.cloud.bigquery.storage.v1.BigQueryWrite.ReadRows" OR
   protoPayload.methodName="google.cloud.bigquery.v2.JobService.InsertJob" OR
   protoPayload.methodName="google.cloud.bigquery.v2.JobService.Query" OR
