ef2bc6f3af8dff166fbd2d258bdd1af621ffa98d
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2025-11-26 16:13:54 +0100

    fmt

--- modules/dataplex/outputs.tf
+++ modules/dataplex/outputs.tf
@@ -10,10 +10,10 @@ output "pubsub_subscription_id" {
 
 output "logging_sink_id" {
   description = "The ID of the logging sink(s)"
-  value = local.has_folders ? module.logging_infrastructure.folder_sink_ids : module.logging_infrastructure.project_sink_ids
+  value       = local.has_folders ? module.logging_infrastructure.folder_sink_ids : module.logging_infrastructure.project_sink_ids
 }
 
 output "logging_sink_writer_identity" {
   description = "The writer identity of the logging sink(s)"
-  value = local.has_folders ? module.logging_infrastructure.folder_sink_writer_identities : module.logging_infrastructure.project_sink_writer_identities
+  value       = local.has_folders ? module.logging_infrastructure.folder_sink_writer_identities : module.logging_infrastructure.project_sink_writer_identities
 }
